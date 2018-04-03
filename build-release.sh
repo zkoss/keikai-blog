e3c5a92d4f15d032488080240094641f6828c885
cor3000 <robertwenzel@potix.com>
2018-04-03 16:37:29 +0800

    don't include release folder in zip upload to test server

--- build-release.sh
+++ build-release.sh
@@ -3,7 +3,8 @@
 rm -rf ./release/*
 
 bundle exec jekyll build -s ./blog -d ./release
-zip -r keikai-blog.zip release/*
-mv keikai-blog.zip release
-
+cd release
+zip -r keikai-blog.zip *
+cd ..
+scp release/keikai-blog.zip zktest@10.1.3.241:keikai-blog-upload
 
