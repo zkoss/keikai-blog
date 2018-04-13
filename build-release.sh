6e55b0383069c7593d532add66ca14b6dd5bd794
cor3000 <robertwenzel@potix.com>
2018-04-13 16:35:24 +0800

    update release upload location

--- build-release.sh
+++ build-release.sh
@@ -6,5 +6,5 @@ bundle exec jekyll build -s ./blog -d ./release
 cd release
 zip -r keikai-blog.zip *
 cd ..
-scp release/keikai-blog.zip zktest@10.1.3.241:keikai-blog-upload
+scp release/keikai-blog.zip zktest@10.1.3.241:keikaiwebsite-upload/keikai-blog/
 
