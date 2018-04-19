dcced616032e7d5860e6b1c82c66d713749aadce
cor3000 <robertwenzel@potix.com>
2018-04-19 20:17:00 +0800

    use specific lessc version

--- build-styles.sh
+++ build-styles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf blog/css/*
 
-lessc less/blog.less blog/css/blog.min.css -x
-lessc less/blog.less blog/css/blog.css
+npx lessc less/blog.less blog/css/blog.min.css -x
+npx lessc less/blog.less blog/css/blog.css
 
 mkdir --parents blog/css/bootstrap
 cp less/bootstrap/bootstrap.css blog/css/bootstrap/
@@ -10,5 +10,5 @@ cp less/bootstrap/bootstrap.min.css blog/css/bootstrap/
 
 mkdir --parents blog/css/lato
 cp -r less/lato/* blog/css/lato
-lessc less/lato/latofonts.css blog/css/lato/latofonts.min.css -x
+npx lessc less/lato/latofonts.css blog/css/lato/latofonts.min.css -x
 
