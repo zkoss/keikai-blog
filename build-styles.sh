273076f56de8f6bf5e7f14b39028aff232f5dd46
hawkchen <hawkchen@potix.com>
2018-08-08 12:55:50 +0800

    add “Keikai Getting Started”

--- build-styles.sh
+++ build-styles.sh
@@ -4,11 +4,10 @@ rm -rf blog/css/*
 npx lessc less/blog.less blog/css/blog.min.css -x
 npx lessc less/blog.less blog/css/blog.css
 
-mkdir --parents blog/css/bootstrap
+mkdir -p blog/css/bootstrap
 cp less/bootstrap/bootstrap.css blog/css/bootstrap/
 cp less/bootstrap/bootstrap.min.css blog/css/bootstrap/
 
-mkdir --parents blog/css/lato
+mkdir -p blog/css/lato
 cp -r less/lato/* blog/css/lato
 npx lessc less/lato/latofonts.css blog/css/lato/latofonts.min.css -x
-
