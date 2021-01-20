ccf69b804a2fa811f1e042f0513ef9389dfdacbf
Robert Wenzel <robertwenzel@potix.com>
2021-01-20 16:02:56 +0800

    enable less math flag to evaluate division, as before

--- build-styles.sh
+++ build-styles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf blog/css/*
 
-npx lessc less/blog.less blog/css/blog.min.css --clean-css
-npx lessc less/blog.less blog/css/blog.css
+npx lessc --math=always less/blog.less blog/css/blog.min.css --clean-css
+npx lessc --math=always less/blog.less blog/css/blog.css
 
 mkdir -p blog/css/bootstrap
 cp less/bootstrap/bootstrap.css blog/css/bootstrap/
@@ -10,8 +10,8 @@ cp less/bootstrap/bootstrap.min.css blog/css/bootstrap/
 
 mkdir -p blog/css/fonts
 cp -r less/fonts/* blog/css/fonts
-npx lessc less/keikaifonts.css blog/css/keikaifonts.min.css --clean-css
+npx lessc --math=always less/keikaifonts.css blog/css/keikaifonts.min.css --clean-css
 
 mkdir -p blog/css/lato
 cp -r less/lato/* blog/css/lato
-npx lessc less/lato/latofonts.css blog/css/lato/latofonts.min.css --clean-css
+npx lessc --math=always less/lato/latofonts.css blog/css/lato/latofonts.min.css --clean-css
